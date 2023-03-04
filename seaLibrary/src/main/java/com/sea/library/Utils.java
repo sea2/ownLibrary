a19ae4aef24567f7a076e9806640a3957eae038c
sea22 <497661292@qq.com>
2023-03-04 13:45:21 +0800

    11

---
+++ seaLibrary/src/main/java/com/sea/library/Utils.java
@@ -0,0 +1,10 @@
+package com.sea.library;
+
+public class Utils {
+
+
+    public static int getAdd(int x, int y) {
+        return x + y;
+    }
+
+}
--- seaLibrary/src/main/java/com/sea/library/download/IRestDataReqNotify.java
+++
@@ -1,5 +0,0 @@
-package com.sea.library.download;
-
-public interface IRestDataReqNotify {	
-	public void TaskNotify(int taskType, String result, int code);
-}
